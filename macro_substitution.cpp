1f668042f4cdfa56629f853ba06380ca8c514fbf
lem <73171983+lefpoem@users.noreply.github.com>
2021-05-30 19:21:13 +0800

    Create macro_substitution.cpp

---
+++ macro_substitution.cpp
@@ -0,0 +1,9 @@
+/*sting const quantization opreator*/
+#include <stdio.h>
+#define message_for(a,b) \   
+   printf(#a " and " #b ":We love you!\n") // "\" macro extension
+int main()
+{
+    message_for(Calore,debran);
+    return 0;
+}
