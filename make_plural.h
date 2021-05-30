29993b798c7789cedac82c859bb91f9209970dad
lem <73171983+lefpoem@users.noreply.github.com>
2021-05-30 19:26:28 +0800

    Create make_plural.h

---
+++ make_plural.h
@@ -0,0 +1,9 @@
+#ifndef MAKE_PLURAL
+#define MAKE_PLURAL
+#include <string>
+using namespace std;
+string make_plural(size_t ct,const string& word,const string ending)
+{
+    return (ct==1)? word:ending;
+}
+#endif
